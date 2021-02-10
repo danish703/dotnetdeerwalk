35f1e83b9adc201775c70acf722d55c24862e5ee
Dipendra <dipu.danish@outlook.com>
2021-02-10 10:29:29 +0545

    formadded

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Day1B.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ViewResult Index()
+        {
+            return View();
+        }
+
+        public String Myfunc(int? id,string name) {
+            return $"id={id} and name={name}";
+        }
+
+        public ViewResult AboutUs()
+        {
+            return View();
+        }
+
+        public ViewResult Contactus()
+        {
+            return View();
+        }
+    }
+}
